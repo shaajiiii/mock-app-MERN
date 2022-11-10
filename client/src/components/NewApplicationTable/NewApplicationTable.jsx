8a8ea2b99c50177cd9626342a6f94098a5be45a3
Manu <manushaji717@gmail.com>
2022-11-11 00:21:08 +0530

    Open button gives a pop up with details. Updations done

--- client/src/components/NewApplicationTable/NewApplicationTable.jsx
+++ client/src/components/NewApplicationTable/NewApplicationTable.jsx
@@ -1,10 +1,13 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Applications-page.css'
 
 function NewApplicationTable() {
 
-    const [applications, setNewApplications] = useState()
+    const [applications, setNewApplications] = useState();
+
+    const [singleApplication, setSingleApplications] = useState();
     
     const getNewApplications = async () => {
         try {
@@ -84,7 +87,10 @@ function NewApplicationTable() {
                                         <td>{singleApplication.companyName}</td>
                                         <td>{singleApplication.phone}</td>
                                         <td>
-                                            <button style={{marginRight:"2rem"}} type="button" class="btn btn-dark ">Open</button>
+                                            <button style={{marginRight:"2rem"}} type="button" class="btn btn-dark "  
+                                            onClick={()=>{setSingleApplications(singleApplication)}}
+                                            
+                                            >Open</button>
                                             <button onClick={()=>{updateToPending(singleApplication._id)}} type="button" class="btn btn-warning">Start Proccess</button>
 
                                         </td>
@@ -103,6 +109,146 @@ function NewApplicationTable() {
 
 
             </div>
+
+
+
+            {/* opened form */}
+
+
+
+
+            {
+                singleApplication &&
+
+
+                <form className='col-12 col-md-8 col-lg-6 application-form' >
+                <h3>Application details</h3>
+                 <div className="row">
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Company Name"
+                            value={singleApplication.companyName}
+                             readOnly
+                             />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Address"
+                            value={singleApplication.address}
+                             readOnly
+                     
+                         />
+
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="City"
+                            value={singleApplication.city}
+                             readOnly
+                       
+                         />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="State"
+                             value={singleApplication.state}
+                             readOnly
+                           
+                         />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Email"
+                            value={singleApplication.email}
+                             readOnly
+                            
+                         />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Phone"
+                            value={singleApplication.phone}
+                             readOnly
+                            
+                         />
+                     </div>
+
+                     <div className="form-group mb-3">
+                         <h6 for="exampleFormControlTextarea1">Team and background</h6>
+                         <textarea
+                             className="form-control"
+                             id="exampleFormControlTextarea1"
+                             rows="1"
+                              value={singleApplication.Describe_team}
+                             readOnly
+                            
+                         ></textarea>
+                     </div>
+                     <div className="form-group mb-3">
+                         <h6 for="exampleFormControlTextarea1">Company and products</h6 >
+                         <textarea
+                             className="form-control"
+                             id="exampleFormControlTextarea1"
+                             rows="1"
+                              value={singleApplication.Describe_company}
+                             readOnly
+                           
+                         ></textarea>
+                     </div>
+                     <div className="form-group mb-3">
+                         <h6 for="exampleFormControlTextarea1">Problem they are trying to solve</h6 >
+                         <textarea
+                             className="form-control"
+                             id="exampleFormControlTextarea1"
+                             rows="1"
+                              value={singleApplication.Describe_problem}
+                             readOnly
+                             
+                         ></textarea>
+                     </div>
+                     <div className="form-group mb-3">
+
+                     <button style={{float:"right"}} type="button" class="btn btn-dark"
+                     onClick={()=>{setSingleApplications(null)}}
+                     >Close</button>
+                        
+                     </div>
+                    
+                    
+
+
+                 </div>
+             </form>
+
+
+            }
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         </>
     )
--- client/src/components/Pending-Application-list/PendingApplicationTable.jsx
+++ client/src/components/Pending-Application-list/PendingApplicationTable.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 function PendingApplicationTable() {
 
     const [pendingApplications, setPendingApplications] = useState();
+    const [singleApplication, setSingleApplications] = useState();
    
 
     const getPendingApplications = async () => {
@@ -77,6 +78,15 @@ function PendingApplicationTable() {
 
 
     return (
+
+
+        <>
+        
+        
+        
+       
+
+
         <div className='pending-table-wrap'>
             <h1 style={{ margin: "1rem 0rem 1rem 0rem " }}> Pending List</h1>
 
@@ -103,7 +113,8 @@ function PendingApplicationTable() {
                                         <td>{singleApplication.companyName}</td>
                                         <td>{singleApplication.phone}</td>
                                         <td>
-                                            <button style={{marginRight:"2rem"}} type="button" class="btn btn-dark ">Open</button>
+                                            <button style={{marginRight:"2rem"}} type="button" class="btn btn-dark "
+                                              onClick={()=>{setSingleApplications(singleApplication)}} >Open</button>
                                            {(singleApplication.status==="pending"?
                                            <>
                                            <button onClick={()=>{if(window.confirm("approve this application?"))updateToApproved(singleApplication._id)}}  type="button" class="btn btn-success mx-2"> Approve </button>
@@ -132,6 +143,145 @@ function PendingApplicationTable() {
             </div>
 
         </div>
+
+
+
+
+
+                        
+            {/* opened form */}
+
+
+
+
+            {
+                singleApplication &&
+
+
+                <form className='col-12 col-md-8 col-lg-6 application-form' >
+                <h3>Application details</h3>
+                 <div className="row">
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Company Name"
+                            value={singleApplication.companyName}
+                             readOnly
+                             />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Address"
+                            value={singleApplication.address}
+                             readOnly
+                     
+                         />
+
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="City"
+                            value={singleApplication.city}
+                             readOnly
+                       
+                         />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="State"
+                             value={singleApplication.state}
+                             readOnly
+                           
+                         />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Email"
+                            value={singleApplication.email}
+                             readOnly
+                            
+                         />
+                     </div>
+                     <div className="col-6 mb-3">
+                         <input
+                             type="text"
+                             className="form-control"
+                             placeholder="Phone"
+                            value={singleApplication.phone}
+                             readOnly
+                            
+                         />
+                     </div>
+
+                     <div className="form-group mb-3">
+                         <h6 for="exampleFormControlTextarea1">Team and background</h6>
+                         <textarea
+                             className="form-control"
+                             id="exampleFormControlTextarea1"
+                             rows="1"
+                              value={singleApplication.Describe_team}
+                             readOnly
+                            
+                         ></textarea>
+                     </div>
+                     <div className="form-group mb-3">
+                         <h6 for="exampleFormControlTextarea1">Company and products</h6 >
+                         <textarea
+                             className="form-control"
+                             id="exampleFormControlTextarea1"
+                             rows="1"
+                              value={singleApplication.Describe_company}
+                             readOnly
+                           
+                         ></textarea>
+                     </div>
+                     <div className="form-group mb-3">
+                         <h6 for="exampleFormControlTextarea1">Problem they are trying to solve</h6 >
+                         <textarea
+                             className="form-control"
+                             id="exampleFormControlTextarea1"
+                             rows="1"
+                              value={singleApplication.Describe_problem}
+                             readOnly
+                             
+                         ></textarea>
+                     </div>
+                     <div className="form-group mb-3">
+
+                     <button style={{float:"right"}} type="button" class="btn btn-dark"
+                     onClick={()=>{setSingleApplications(null)}}
+                     >Close</button>
+                        
+                     </div>
+                    
+                    
+
+
+                 </div>
+             </form>
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        </>
     )
 }
 
